19cd044b08f1361440494a6e863b7914d98239de
Viviana Leiva Galvez <vivianaleivagalvez@MacBook-Pro-de-William.local>
2018-03-22 23:56:52 -0300

    segunda subida

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  constructor(){
+    this.message = "hola";
   }
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+
+    // config.map([
+    //   { route: ['','home'],  name: 'home',  
+    //     moduleId: './home',  nav: true, title:'Home' }
+    // ]);
+    // this.router = router;
+
+  }
+
+
 }
